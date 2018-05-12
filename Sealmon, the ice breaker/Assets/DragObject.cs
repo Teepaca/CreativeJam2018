81c0ac5b018f071e4b85d48d8073ad21f3af2a0a
MichaelSilva13 <ferry.francois1504@orange.fr>
2018-05-12 14:31:11 -0400

    Initial commit

---
+++ Sealmon, the ice breaker/Assets/DragObject.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+
+    public Vector3 screenPoint;
+    Vector3 offset;
+    Vector3 scanPos;
+    Transform trans;
+
+    public float maxX;
+    public float maxY;
+
+    void Start()
+    {
+        trans = this.transform;
+
+        scanPos = trans.position;
+
+    }
+
+    void Update(){}
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(scanPos);
+        offset = scanPos - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, 
+                                                                      Input.mousePosition.y, 
+                                                                      screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        trans.position = curPosition;
+        float posX = Mathf.Clamp(trans.position.x, - maxX, maxX);
+        float posY = Mathf.Clamp(trans.position.y, - maxY, maxY);
+        trans.position = new Vector3(posX, posY, curPosition.z);
+    }
+
+    void OnMouseUp()
+    {
+        trans.position = scanPos;
+    }
+}
+    
---
+++ Sealmon, the ice breaker/Assets/SlingShotManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShotManager : MonoBehaviour
+{
+
+    public Transform leftAnchor;
+    public Transform rightAnchor;
+    public Transform ObjectHolder;
+    public LineRenderer[] lines;
+
+    public static SlingShotManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+
+    void Start()
+    {
+        lines[0].SetPosition(0, leftAnchor.position);
+        lines[1].SetPosition(0, rightAnchor.position);
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        for (int i = 0; i < lines.Length; i++)
+        {
+            lines[i].SetPosition(1, ObjectHolder.position);
+        }
+    }
+ }
